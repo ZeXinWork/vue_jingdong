65ec3e7277d8db8465791aaca4ff2dc60f38fb0b
zhengzexin <zhengzexin@xmwefun.com>
2021-08-26 13:58:23 +0800

    feat: 抽离组件shopInfo

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,35 +1,21 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div class="nearby__item" v-for="item of nearbyList" :key="item._id">
-      <img :src="item.imgUrl" class="nearby__item__img" />
-      <div class="nearby__content">
-        <div class="nearby__content__title">{{ item.name }}</div>
-        <div class="nearby__content__tags">
-          <span class="nearby__content__tag">月售{{ item.sales }}+</span>
-          <span class="nearby__content__tag">
-            起送￥{{ item.expressPrice }}
-          </span>
-          <span class="nearby__content__tag">
-            配送费{{ item.expressLimit }}
-          </span>
-        </div>
-        <p class="nearby__content__highlight">
-          {{ item.slogan }}
-        </p>
-      </div>
-    </div>
+    <ShopInfo v-for="item of nearbyList" :key="item._id" :shop="item" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo'
 export default {
   name: 'Nearby',
+  components: {
+    ShopInfo
+  },
   setup() {
     const nearbyList = ref([])
-
     const getNearbyList = async () => {
       const result = await get('/api/shop/hot-list')
       if (result.errno === 0) {
@@ -50,40 +36,6 @@ export default {
     font-size: 0.18rem;
     font-weight: normal;
     color: $content-fontcolor;
-  }
-  &__item {
-    display: flex;
-    padding-top: 0.12rem;
-    &__img {
-      margin-right: 0.16rem;
-      width: 0.56rem;
-      height: 0.56rem;
-    }
-  }
-  &__content {
-    flex: 1;
-    padding-bottom: 0.12rem;
-    border-bottom: 1px solid $content-bgcolor;
-    &__title {
-      line-height: 0.22rem;
-      font-size: 0.16rem;
-      color: $content-fontcolor;
-    }
-    &__tags {
-      margin-top: 0.08rem;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: $content-fontcolor;
-    }
-    &__tag {
-      margin-right: 0.16rem;
-    }
-    &__highlight {
-      margin: 0.08rem 0 0 0;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: #e93b3b;
-    }
   }
 }
 </style>
